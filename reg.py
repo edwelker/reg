2501e12e940ab19e2ed51efb74ddc242bc83be1f
Eddie Welker <welkere@ncbi.nlm.nih.gov>
2016-02-08 00:29:53 -0500

    mount session, tho i think this would work without me doing anything

--- reg.py
+++ reg.py
@@ -1,29 +1,12 @@
 import os
 import time
-import uwsgi
 import requests
 import fcntl
 from uwsgidecorators import *
 
-# s = requests.Session()
-
-# def register(signum):
-#     start_time = time.time()
-#     print("**********************here is the signum: {}".format(signum))
-#     s.get("http://www.ncbi.nlm.nih.gov")
-#     print("--- %s seconds ---" % (time.time() - start_time))
-
-# uwsgi.register_signal(99, "", register)
-# uwsgi.add_timer(99, 3)
-
-
-# @postfork
-# def pf():
-#     """This returns 1 each time, b/c each post-fork is in a different thread/process"""
-#     global x
-#     x = x + 1
-#     print("Post-fork hook called, time number {}".format(x))
-
+session = requests.Session()
+adapter = requests.adapters.HTTPAdapter(max_retries=2, pool_maxsize=4, pool_connections=1)
+session.mount('https://www.ncbi.nln.nih.gov', adapter)
 
 @postfork
 def pf():
@@ -31,9 +14,11 @@ def pf():
     try:
         fcntl.flock(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
         print("Got the lock on pid: {}".format(os.getpid()))
-        for x in xrange(0,10):
+        for x in xrange(0,4):
             print("Doing work, time {}, on pid: {}".format(x, os.getpid()))
-            time.sleep(1)
+            print(session.get('https://www.ncbi.nlm.nih.gov/').status_code)
+            # really? boo
+            time.sleep(1.5)
     except IOError, e:
         print("Unable to retrieve lock for fileno {}, pid: {}".format(f.fileno(), os.getpid()))
     finally:
