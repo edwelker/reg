22c47e8d89bf4b4a658d4b96f6b449a4d728d367
Eddie Welker <welkere@ncbi.nlm.nih.gov>
2016-02-08 19:39:46 -0500

    pre-add lbos

--- register.py
+++ register.py
@@ -1,8 +1,6 @@
 import os
 import time
 import requests
-import fcntl
-from uwsgidecorators import *
 
 session = requests.Session()
 adapter = requests.adapters.HTTPAdapter(max_retries=2, pool_maxsize=4, pool_connections=1)
@@ -10,17 +8,11 @@ session.mount('https://www.ncbi.nln.nih.gov', adapter)
 
 
 def register_loop():
-    registered = False
-    while registered is not True:
-        # message = uwsgi.mule_get_msg()
-        print("registering {} from process {}, signal: ".format(os.getenv("LBOSADDRESS"), os.getpid()))
-        registered = True
-        time.sleep(10)
-
-# uwsgi.register_signal(1, "mule1", register_loop)
-
+    while True:
+        print("registering {} from process {}".format(os.getenv("LBOSADDRESS"), os.getpid()))
+        # wait for 15m to re-register
+        time.sleep(900)
 
 
 if __name__ == '__main__':
     register_loop()
-    print("Done with pid: {}".format(os.getpid()))
